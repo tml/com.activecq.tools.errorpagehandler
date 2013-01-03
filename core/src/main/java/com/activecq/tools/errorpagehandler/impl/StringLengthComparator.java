44d906dfb758c044fbd4d05f786d5ac9478ba76e
david <github@activecq.com>
2013-01-02 19:03:10 -0500

    Added correct copyright information

--- core/src/main/java/com/activecq/tools/errorpagehandler/impl/StringLengthComparator.java
+++ core/src/main/java/com/activecq/tools/errorpagehandler/impl/StringLengthComparator.java
@@ -1,13 +1,30 @@
 /*
+ * Copyright 2013 david gonzalez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 package com.activecq.tools.errorpagehandler.impl;
 
-import java.util.Comparator;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Comparator;
 
 /**
  *
